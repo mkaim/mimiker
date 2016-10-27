8c9c794371dd9fa136b698689c28c3dc9c9ef39f
coodie <poppinggoovie@gmail.com>
2016-10-28 00:35:11 +0200

    Add td_type_t flags

--- sched.c
+++ sched.c
@@ -22,11 +22,11 @@ static void demo_thread_2() {
 int main() {
   sched_init();
 
-  thread_t *t1 = thread_create("t1", demo_thread_1);
-  thread_t *t2 = thread_create("t2", demo_thread_1);
-  thread_t *t3 = thread_create("t3", demo_thread_1);
-  thread_t *t4 = thread_create("t4", demo_thread_2);
-  thread_t *t5 = thread_create("t5", demo_thread_2);
+  thread_t *t1 = thread_create("t1", demo_thread_1, TD_KERNEL);
+  thread_t *t2 = thread_create("t2", demo_thread_1, TD_KERNEL);
+  thread_t *t3 = thread_create("t3", demo_thread_1, TD_KERNEL);
+  thread_t *t4 = thread_create("t4", demo_thread_2, TD_KERNEL);
+  thread_t *t5 = thread_create("t5", demo_thread_2, TD_KERNEL);
 
   sched_add(t1);
   sched_add(t2);
