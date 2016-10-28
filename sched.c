b461eb6dd3ebcce5a5e69cad05086b609983c264
coodie <poppinggoovie@gmail.com>
2016-10-29 01:29:35 +0200

    sys/exec.c

--- sched.c
+++ sched.c
@@ -1,3 +1,4 @@
+#include <vm_pager.h>
 #include <stdc.h>
 #include <clock.h>
 #include <thread.h>
@@ -19,7 +20,8 @@ static void demo_thread_2() {
   demo_thread_1();
 }
 
-int main() {
+void thread_test()
+{
   sched_init();
 
   thread_t *t1 = thread_create("t1", demo_thread_1, TD_KERNEL);
@@ -35,4 +37,62 @@ int main() {
   sched_add(t5);
 
   sched_run();
+}
+
+#define USER_START 0x1234000
+#define USER_END 0x1235000
+
+#define KERNEL_START 0xc1230000
+#define KERNEL_END   0xc1231000
+
+void kernel_thread()
+{
+    volatile int *ptr = (int*)(KERNEL_START);
+    while(1){
+        *ptr = *ptr+1;
+        kprintf("thread: %s, val: %d\n", thread_self()->td_name, *ptr);
+        sched_yield();
+    }
+}
+
+void user_thread()
+{
+    volatile int *ptr = (int*)(USER_START);
+    while(1){
+        *ptr = *ptr+1;
+        kprintf("thread: %s, val: %d\n", thread_self()->td_name, *ptr);
+        sched_yield();
+    }
+}
+
+void kernel_and_user_threads() {
+  thread_t *t1 = thread_create("kern_1", kernel_thread, TD_KERNEL);
+  thread_t *t2 = thread_create("kern_2", kernel_thread, TD_KERNEL);
+  thread_t *t3 = thread_create("user_1", user_thread, TD_USER);
+  thread_t *t4 = thread_create("user_2", user_thread, TD_USER);
+
+  vm_map_entry_t *entry1 = vm_map_add_entry(get_active_vm_map(PMAP_KERNEL), KERNEL_START, KERNEL_END, VM_PROT_READ | VM_PROT_WRITE);
+  entry1->object = default_pager->pgr_alloc();
+
+  vm_map_t *map1 = t3->user_map;
+  set_active_vm_map(t3->user_map);
+  vm_map_entry_t *entry2 = vm_map_add_entry(map1, USER_START, USER_END, VM_PROT_READ | VM_PROT_WRITE);
+  entry2->object = default_pager->pgr_alloc();
+
+  set_active_vm_map(t4->user_map);
+  vm_map_t *map2 = t4->user_map;
+  vm_map_entry_t *entry3 = vm_map_add_entry(map2, USER_START, USER_END, VM_PROT_READ | VM_PROT_WRITE);
+  entry3->object = default_pager->pgr_alloc();
+
+  sched_add(t1);
+  sched_add(t2);
+  sched_add(t3);
+  sched_add(t4);
+
+  sched_run();
+}
+
+int main() {
+  kernel_and_user_threads();
+  return 0;
 }
